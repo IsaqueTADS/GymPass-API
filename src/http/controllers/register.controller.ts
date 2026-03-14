a45b22ca094f54c6b61898d808fde723a30e79c1
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-13 23:33:18 -0300

    feat: implement password hashing in user registration controller using argon2

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,3 +1,4 @@
+import { hash } from 'argon2'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '@/lib/prisma.js'
 import { registerBodySchema } from '../routes.js'
@@ -5,11 +6,23 @@ import { registerBodySchema } from '../routes.js'
 export async function resgister(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
