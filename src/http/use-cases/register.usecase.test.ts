4926f09efc7d7cc4698176486ba72eaccd0c8fc6
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-14 21:29:21 -0300

    test: expand RegisterUseCase tests to include duplicate email handling and successful registration

--- src/http/use-cases/register.usecase.test.ts
+++ src/http/use-cases/register.usecase.test.ts
@@ -1,23 +1,14 @@
 import { verify } from 'argon2'
 import { describe, expect, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository.js'
+import { UserAlreadyExistsError } from './errors/user-already-exist-error.js'
 import { RegisterUseCase } from './register-usecase.js'
 
 describe('Register UseCase', () => {
   it('A senha do usuário precisa estar criptografada', async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(_email) {
-        return null
-      },
-      async create(data) {
-        return {
-          id: 'gbytjfdaskhjdapHJDPai',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        }
-      },
-    })
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
 
     const password = '1234567'
 
@@ -30,5 +21,44 @@ describe('Register UseCase', () => {
     const isPasswordHashValid = await verify(user.password_hash, password)
 
     expect(isPasswordHashValid).toBe(true)
+  })
+
+  it('O usuário não deve se cadastrar como um e-mail duplicado', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const password = '1234567'
+    const email = 'teste@gmail.com'
+
+    await registerUseCase.execute({
+      name: 'Testador',
+      email,
+      password,
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'Testador',
+        email,
+        password,
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+  it('Deve ser possivel se cadastrar', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const password = '1234567'
+    const email = 'teste@gmail.com'
+
+    const { user } = await registerUseCase.execute({
+      name: 'Testador',
+      email,
+      password,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
   })
 })
