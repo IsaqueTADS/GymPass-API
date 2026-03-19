9553d952d2eb9db12690125507e819eced099c30
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-19 00:02:23 -0300

    feat: add image_url field to CreateUserDTO, ResponseUserDTO, and User interfaces

--- src/dtos/user/response-user.dto.ts
+++ src/dtos/user/response-user.dto.ts
@@ -2,5 +2,6 @@ export interface ResponseUserDTO {
   id: string
   name: string
   email: string
+  image_url: string | null
   created_at: Date
 }
--- src/dtos/user/user.ts
+++ src/dtos/user/user.ts
@@ -2,6 +2,7 @@ export interface User {
   id: string
   name: string
   email: string
+  image_url: string | null
   password_hash: string
   created_at: Date
 }
