d539baf47f743ace875e7b4f5a0283f57715087c
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-19 17:40:14 -0300

    feat: implement create method in InMemoryGymsRepository for gym creation using CreateGymDTO

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,8 @@
+import type { CreateGymDTO } from '@/dtos/gyms/create-gym.dto.js'
 import type { Gym } from '@/dtos/gyms/gym.js'
 
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  create(data: CreateGymDTO): Promise<Gym>
 }
+                                                           
--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,9 +1,25 @@
+import { randomUUID } from 'node:crypto'
+import type { CreateGymDTO } from '@/dtos/gyms/create-gym.dto.js'
 import type { Gym } from '@/dtos/gyms/gym.js'
-
 import type { GymsRepository } from '../gyms-repository.js'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
+
+  async create(data: CreateGymDTO): Promise<Gym> {
+    const gym = {
+      id: randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone,
+      latitude: data.latitude,
+      longitude: data.longitude,
+    } 
+
+    this.items.push(gym)
+
+    return gym
+  }
 
   async findById(id: string): Promise<Gym | null> {
     const gym = this.items.find((item) => item.id === id)
