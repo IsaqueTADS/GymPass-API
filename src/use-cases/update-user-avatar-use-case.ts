0df2b7b37d321e14e237da218c327c702f42e32f
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-19 05:46:04 -0300

    feat: add UpdateUserAvatarUseCase and include implementation plans in .gitignore

---
+++ src/use-cases/update-user-avatar-use-case.ts
@@ -0,0 +1,7 @@
+import type { usersRepository } from '@/repositories/users-repository.js'
+
+class UpadateUserAvatarUseCase {
+  constructor(private userRepository: usersRepository) {}
+
+  async execute() {}
+}
