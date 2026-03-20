11823577ece53e12181559b5a7a7410d55c573cb
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-20 18:27:04 -0300

    test: add pagination test for FetchUserCheckInsHistoryUseCase to verify correct check-in history retrieval

--- src/use-cases/fetch-user-check-ins-history-use-case.test.ts
+++ src/use-cases/fetch-user-check-ins-history-use-case.test.ts
@@ -23,12 +23,33 @@ describe('Fetch user check ins historyr', () => {
       gym_id: 'gym-2',
     })
 
-    const { checkIns } = await sut.execute({ userId })
+    const { checkIns } = await sut.execute({ userId, page: 1 })
 
     expect(checkIns).toHaveLength(2)
     expect(checkIns).toEqual([
       expect.objectContaining({ gym_id: 'gym-1' }),
       expect.objectContaining({ gym_id: 'gym-2' }),
+    ])
+  })
+  it('Deve ser possivel obter busca paginada  do historico de check-ins', async () => {
+    const userId = randomUUID()
+
+    for (let i = 1; i <= 25; i++) {
+      inMemoryCheckInsRepository.create({
+        user_id: userId,
+        gym_id: `gym-${i}`,
+      })
+    }
+
+    const { checkIns } = await sut.execute({ userId, page: 2 })
+
+    expect(checkIns).toHaveLength(5)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
+      expect.objectContaining({ gym_id: 'gym-23' }),
+      expect.objectContaining({ gym_id: 'gym-24' }),
+      expect.objectContaining({ gym_id: 'gym-25' }),
     ])
   })
 })
