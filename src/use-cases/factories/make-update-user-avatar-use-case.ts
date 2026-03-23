f613a27c1b50415b04228d62c9c60885caad3e8e
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-23 15:17:52 -0300

    refactor: replace InMemoryUploadGateway with UploadClaudinaryGateway in update user avatar use case factory

--- src/use-cases/factories/make-update-user-avatar-use-case.ts
+++ src/use-cases/factories/make-update-user-avatar-use-case.ts
@@ -1,11 +1,10 @@
-
-import { InMemoryUploadGateway } from '@/gateways/in-memory/in-memory-upload-gateway.js'
+import { UploadClaudinaryGateway } from '@/gateways/claudinary/upload-claudinary-gateway.js'
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository.js'
 import { UpadateUserAvatarUseCase } from '../update-user-avatar-use-case.js'
 
 export function makeUpdateUserAvatar() {
   const userRepository = new PrismaUserRepository()
-  const uploadGataway = new InMemoryUploadGateway()
+  const uploadGataway = new UploadClaudinaryGateway()
 
   const updateUserAvatar = new UpadateUserAvatarUseCase(
     userRepository,
