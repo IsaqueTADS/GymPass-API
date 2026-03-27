95ed1410a2b57810db9950f111d75d87ee295129
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-27 12:08:31 -0300

    refactor: streamline database setup in Prisma test environment and improve teardown process

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,41 +1,18 @@
-import 'dotenv/config'
-
 import { execSync } from 'node:child_process'
-import { randomUUID } from 'node:crypto'
 import type { Environment } from 'vitest/runtime'
 import { prisma } from '@/lib/prisma.js'
-
-function generateDatabaseURL(schema: string) {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL not found')
-  }
-
-  const url = new URL(process.env.DATABASE_URL)
-  url.searchParams.set('schema', schema)
-  return url.toString()
-}
 
 export default (<Environment>{
   name: 'custom',
   viteEnvironment: 'ssr',
 
   async setup() {
-    const schema = randomUUID()
-    const databaseUrl = generateDatabaseURL(schema)
-
-    process.env.DATABASE_URL = databaseUrl
-   
     execSync('npm run db:migrate:deploy')
-
     return {
       async teardown() {
-        console.log('[TestEnv] Dropping schema:', schema)
-        
-
-        await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
-        )
-        await prisma.$disconnect()
+        await prisma.$executeRawUnsafe('DELETE FROM users')
+        await prisma.$executeRawUnsafe('DELETE FROM gyms')
+        await prisma.$executeRawUnsafe('DELETE FROM check_ins')
       },
     }
   },
