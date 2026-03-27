708210876f59376c8825d89efcd8449ed509efc6
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-27 00:01:09 -0300

    refactor: remove unused imports and clean up validate check-in use case

--- src/use-cases/validate-check-in-use-case.ts
+++ src/use-cases/validate-check-in-use-case.ts
@@ -1,11 +1,9 @@
 import dayjs from 'dayjs'
 import type { CheckIn } from '@/dtos/checkin/checkin.js'
 import type { CheckInsRepository } from '@/repositories/check-ins-repository.js'
-import type { GymsRepository } from '@/repositories/gyms-repository.js'
-import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates.js'
+
 import { LateValidationCheckInsError } from './errors/late-validation-check-ins-error.js'
-import { MaxDistanceError } from './errors/max-distance-error.js'
-import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error.js'
+
 import { ResourceNotFoundError } from './errors/resource-not-found-error.js'
 
 interface ValidateCheckInUseCaseRequest {
